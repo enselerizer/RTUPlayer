a2e7be89171e2f7e04bc3b259f26f3ebe6a1dadf
EnselerizeR <enselerizer@gmail.com>
2020-02-03 02:50:52 +0300

    Basic design, menu page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
 import {VgCoreModule} from 'videogular2/compiled/core';
 import {VgControlsModule} from 'videogular2/compiled/controls';
 import {VgOverlayPlayModule} from 'videogular2/compiled/overlay-play';
 import {VgBufferingModule} from 'videogular2/compiled/buffering';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { MainPageBlockVideoComponent } from './main-page-block-video/main-page-block-video.component';
+import { MainPageBlockImageComponent } from './main-page-block-image/main-page-block-image.component';
+import { SafePipe } from './safe.pipe';
+
+const appRoutes: Routes =[
+  { path: '', component: MainPageComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    MainPageBlockVideoComponent,
+    MainPageBlockImageComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserModule,
     VgCoreModule,
     VgControlsModule,
     VgOverlayPlayModule,
-    VgBufferingModule
+    VgBufferingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
